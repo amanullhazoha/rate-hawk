b9dfe54a318ee6dc269954cb1814093ddd45db28
amanullhazoha <amanullhazoha3784@gmail.com>
2024-07-03 20:48:22 +0600

    build issue solve

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,11 +1,20 @@
 import UserLayout from "@/components/layoutSection/UserLayout";
+import { Suspense } from "react";
 
 const Layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <UserLayout>{children}</UserLayout>;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-20">loading....</div>
+      }
+    >
+      <UserLayout>{children}</UserLayout>;
+    </Suspense>
+  );
 };
 
 export default Layout;
